f0aaa6616035a8486e53f73b99615ddec281b929
Gustavo Saiani <gustavo@poe.ma>
2016-05-02 10:32:56 -0300

    Install Guardian

--- web/router.ex
+++ web/router.ex
@@ -9,6 +9,18 @@ defmodule GusaianiPhoenixTrello.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", PhoenixTrello do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registrations", RegistrationController, :create
+    end
+  end
+
   scope "/", GusaianiPhoenixTrello do
     pipe_through :browser # Use the default browser stack
 
