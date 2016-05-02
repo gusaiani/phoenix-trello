f5436e4854ca2fc3239f4d25953fd109c607be80
Gustavo Saiani <gustavo@poe.ma>
2016-05-02 10:24:36 -0300

    User model

--- mix.exs
+++ mix.exs
@@ -18,8 +18,17 @@ defmodule GusaianiPhoenixTrello.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {GusaianiPhoenixTrello, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+      applications: [
+        :phoenix,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :postgrex,
+        :comeonin
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -36,7 +45,8 @@ defmodule GusaianiPhoenixTrello.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 2.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
