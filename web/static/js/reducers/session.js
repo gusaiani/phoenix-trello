21be82cb48ddd8fef458a6369a924396480cd137
Gustavo Saiani <gustavo@poe.ma>
2016-05-09 10:31:27 -0300

    Add sign_out and error cases to session reducer

--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -2,12 +2,21 @@ import Constants from '../constants'
 
 const initialState = {
   currentUser: null,
+  socket: null,
+  channel: null,
+  error: null,
 }
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case Constants.CURRENT_USER:
       return { ...state, currentUser: action.currentUser }
+
+    case Constants.USER_SIGNED_OUT:
+      return initialState
+
+    case Constants.SESSIONS_ERROR:
+      return { ...state, error: action.error }
 
     default:
       return state
