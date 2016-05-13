b2707969f1a06789699aa4e87e347940dba03f55
Gustavo Saiani <gustavo@poe.ma>
2016-05-13 14:29:36 -0300

    List model

---
+++ test/models/list_test.exs
@@ -0,0 +1,18 @@
+defmodule GusaianiPhoenixTrello.ListTest do
+  use GusaianiPhoenixTrello.ModelCase
+
+  alias GusaianiPhoenixTrello.List
+
+  @valid_attrs %{name: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = List.changeset(%List{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = List.changeset(%List{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
